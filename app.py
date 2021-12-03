527e4fc8e5d482ae73739156beb8a432aff6248d
RuitaoWu <ruitaowu0@gmail.com>
2021-12-03 12:16:18 -0800

    update and eliminate the conflict with merged code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+# <<<<<<< HEAD
 from flask import Flask, render_template, Response
 import objRealTimeDectector as obj
 #flask
@@ -16,9 +16,9 @@ def video_feed():
     return Response(obj.videoDetector(),mimetype="multipart/x-mixed-replace; boundary=frame")
 if __name__ == "__main__":
     app.run(debug=True)
-=======
-from mysql.connector import MySQLConnection, Error
-from config import read_db_config
+# =======
+# from mysql.connector import MySQLConnection, Error
+# from config import read_db_config
 from flask import Flask, render_template, Response
 import objRealTimeDectector as obj
 #flask
@@ -36,4 +36,4 @@ def video_feed():
     return Response(obj.videoDetector(),mimetype="multipart/x-mixed-replace; boundary=frame")
 if __name__ == "__main__":
     app.run(debug=True)
->>>>>>> 9c6d4a331b478291cf2f1ce9e14e47a0374b3733
+# >>>>>>> 9c6d4a331b478291cf2f1ce9e14e47a0374b3733
