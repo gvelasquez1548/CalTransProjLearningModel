9617c3a3ba446ed013d341c3af851d317c01ab0e
CesarV97 <cesar_anvipe@live.com.mx>
2021-12-13 02:43:14 -0800

    Database
    Database

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # <<<<<<< HEAD
 from flask import Flask, render_template, Response
 import objRealTimeDectector as obj
+from config import read_db_config
+
 #flask
 app = Flask(__name__)
 #camera
@@ -37,3 +39,20 @@ def video_feed():
 if __name__ == "__main__":
     app.run(debug=True)
 # >>>>>>> 9c6d4a331b478291cf2f1ce9e14e47a0374b3733
+
+def connect():
+    db_config = read_db_config()
+    conn = None
+    try:
+        print('Connecting to MySQL database')
+        conn = MySQLConnection(**db_config)
+
+        if conn.is_connected():
+            print('Connection established')
+        else:
+            print('Connection failed')
+    
+    except Error as error:
+        print(error)
+
+    return conn
